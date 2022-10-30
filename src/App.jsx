d1748be766b8f4c6b1828b0aad5e559df56a3f4d
MaximilianoCoceres <maximilianococeres7@gmail.com>
2022-10-30 16:59:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { MoviesGrid } from "./moviesGrid";
+import styles from "./app.module.css"
+
+
+export function App(){
+    return <div>
+        <header>
+            <h1 className={styles.title}>Movies</h1>
+        </header>
+        <main>
+            <MoviesGrid/>
+        </main>
+    </div>
+
+}
+
